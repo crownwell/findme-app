c04eb16ae6628372a1b6b29b7498be43c5205b90
GIDEON PETERS <gideonpeters85@gmail.com>
2019-10-31 08:01:36 +0100

    made some changes

---
+++ lib/screens/user/user_wallet.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class UserWallet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text('USer Wallet Page');
+  }
+}
